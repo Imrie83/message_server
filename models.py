f96d53e994823354f24f020e4e14c099f36300f4
Imrie83 <czolgista83@gmail.com>
2021-11-04 13:22:59 +0100

    Prepared space for required methods - to be filled

--- models.py
+++ models.py
@@ -13,4 +13,24 @@ class Users:
     def hashed_password(self):
         return self._hashed_password
 
-    
+    @new_password.setter
+    def new_password(self, new_pass):
+        self._hashed_password = new_pass    # TODO set up password hashing in setter!
+
+    def save_to_db(self, cursor):           # TODO set method adding elements to table
+        pass
+
+    @staticmethod
+    def load_user_by_name(cursor):          # TODO set method loading users by name
+        pass
+
+    @staticmethod
+    def load_user_by_id(cursor):            # TODO set method loading users by id
+        pass
+
+    @staticmethod
+    def load_all_users(cursor):             # TODO set method loading all users
+        pass
+
+    def delete_user(self, cursor):          # TODO set method deleting user
+        pass
