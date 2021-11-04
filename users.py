3b22b7c22e7471bd15444479793ec02056baee96
Imrie83 <czolgista83@gmail.com>
2021-11-04 19:46:56 +0100

    Created main body of Users script. Needs some cleaning to do

--- users.py
+++ users.py
@@ -8,14 +8,6 @@ PASSWORD = 'coderslab'
 HOST = 'localhost'
 DB = 'message_db'
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--username', help='username')
-parser.add_argument('-p', '--password', help='password - minimum 8 characters')
-parser.add_argument('-n', '--new_pass', help='create new password')
-parser.add_argument('-l', '--list', help='user list')
-parser.add_argument('-d', '--delete', help='delete user')
-parser.add_argument('-e', '--edit', help='edit user')
-
 # display parser help
 # parser.print_help()
 
@@ -41,7 +33,7 @@ def create_user(username, password):
             new_user.save_to_db(cursor)
         except UniqueViolation:
             return 'User already exist - Username must be UNIQUE'
-        return new_user
+        return 'User created successfully'
 
 
 def edit_password(username, password, new_pass=''):
@@ -59,7 +51,7 @@ def edit_password(username, password, new_pass=''):
         return 'User does not exist in database'
 
     elif not user.hashed_password == password:
-        return 'Incorrect Passowrd'
+        return 'Incorrect Password'
 
     elif len(new_pass) < 8:
         return 'New password too short - minimum 8 characters required'
@@ -113,24 +105,42 @@ Username: {user.username}
 
 # TODO: Set parser help on incorrect argument
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--username', help='username')
+    parser.add_argument('-p', '--password', help='password - minimum 8 characters')
+    parser.add_argument('-n', '--new_pass', help='create new password')
+    parser.add_argument('-l', '--list', help='user list', action='store_true')
+    parser.add_argument('-d', '--delete', help='delete user', action='store_true')
+    parser.add_argument('-e', '--edit', help='edit user', action='store_true')
+    args = parser.parse_args()
+
+    # TODO: clean argument parsing!
+    if args.list:
+        list_all_users()
+    if args.username and args.password and not args.edit and not args.delete:
+        print(create_user(args.username, args.password))
+    if args.username and args.password and args.edit and args.new_pass:
+        print(edit_password(args.username, args.password, args.new_pass))
+    if args.username and args.password and args.delete:
+        print(delete_user(args.username, args.password))
 
 
-# Tests
-# TODO: Learn how to write proper unittests...
-some_user = create_user('TANK99', 'password_2')
-try:
-    print(some_user)
-    print(some_user.username, some_user.user_id)
-except Exception as e:
-    print(e)
-
-print(edit_password('TANK3', 'password'))
-print(edit_password('TANK3', 'passwor'))
-print(edit_password('TANK4', 'test'))
-print(edit_password('TANK3', 'password', 'some_new_pass'))
-print(delete_user('TANK99', 'pass'))
-print(delete_user('TANK98', 'pass'))
-print(delete_user('TANK99', 'password_2'))
-print('*'*50)
-list_all_users()
-print(parse_help())
+# # Tests
+# # TODO: Learn how to write proper unittests...
+# some_user = create_user('TANK99', 'password_2')
+# try:
+#     print(some_user)
+#     print(some_user.username, some_user.user_id)
+# except Exception as e:
+#     print(e)
+#
+# print(edit_password('TANK3', 'password'))
+# print(edit_password('TANK3', 'passwor'))
+# print(edit_password('TANK4', 'test'))
+# print(edit_password('TANK3', 'password', 'some_new_pass'))
+# print(delete_user('TANK99', 'pass'))
+# print(delete_user('TANK98', 'pass'))
+# print(delete_user('TANK99', 'password_2'))
+# print('*'*50)
+# list_all_users()
