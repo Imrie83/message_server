f44510f817d98b87ed4b2278f9fd71397c397831
Imrie83 <czolgista83@gmail.com>
2021-11-04 18:46:13 +0100

    Created "delete_user" function

--- main.py
+++ main.py
@@ -18,8 +18,11 @@ parser.add_argument('-e', '--edit', help='edit user')
 
 # display parser help
 # parser.print_help()
+
 # TODO Add exception handling
 # TODO Add additionall functionality
+# TODO create class to connecto to database!
+
 
 def create_user(username, password):
 
@@ -56,7 +59,7 @@ def edit_password(username, password, new_pass=''):
         return 'User does not exist in database'
 
     elif not user.hashed_password == password:
-        return 'Password incorrect'
+        return 'Incorrect Passowrd'
 
     elif len(new_pass) < 8:
         return 'New password too short - minimum 8 characters required'
@@ -68,13 +71,41 @@ def edit_password(username, password, new_pass=''):
     return 'Password changed successfully'
 
 
-# Tests
-# some_user = create_user('TANK3', 'password')
-# print(some_user)
-# print(some_user.username, some_user.user_id)
+def delete_user(username, password):
 
+    try:
+        conn = connect(user=USER, password=PASSWORD, host=HOST, dbname=DB)
+        conn.autocommit = True
+        cursor = conn.cursor()
+    except OperationalError as e:
+        return 'Connection error', e
+
+    user = Users.load_user_by_name(cursor, username)
+
+    if not user:
+        return 'User does not exist'
+
+    elif not user.hashed_password == password:
+        return 'Incorrect Password'
+
+    else:
+        user.delete_user(cursor)
+        return 'User has been deleted.'
+
+
+# Tests
+# TODO: Learn how to write proper unittests...
+some_user = create_user('TANK99', 'password_2')
+try:
+    print(some_user)
+    print(some_user.username, some_user.user_id)
+except Exception as e:
+    print(e)
 
 print(edit_password('TANK3', 'password'))
 print(edit_password('TANK3', 'passwor'))
 print(edit_password('TANK4', 'test'))
 print(edit_password('TANK3', 'password', 'some_new_pass'))
+print(delete_user('TANK99', 'pass'))
+print(delete_user('TANK98', 'pass'))
+print(delete_user('TANK99', 'password_2'))
