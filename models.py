f6819451e98629df75959f85f0851330894fb51e
Imrie83 <czolgista83@gmail.com>
2021-11-04 14:03:58 +0100

    save_to_database method working correctly

--- models.py
+++ models.py
@@ -13,12 +13,23 @@ class Users:
     def hashed_password(self):
         return self._hashed_password
 
-    @new_password.setter
-    def new_password(self, new_pass):
+    @hashed_password.setter
+    def hashed_password(self, new_pass):
         self._hashed_password = new_pass    # TODO set up password hashing in setter!
 
     def save_to_db(self, cursor):           # TODO set method adding elements to table
-        pass
+        if self._id == -1:
+            sql = "INSERT INTO users(username, hashed_password) VALUES (%s, %s) RETURNING id"
+            values = (self.username, self._hashed_password)
+            cursor.execute(sql, values)
+            self._id = cursor.fetchone()[0]
+            return True
+        else:
+            sql = "UPDATE users SET username = %s, hashed_password = %s WHERE id = %s"
+            values = (self.username, self._hashed_password, self._id)
+            cursor.execute(sql, values)
+            return True
+
 
     @staticmethod
     def load_user_by_name(cursor):          # TODO set method loading users by name
@@ -34,3 +45,30 @@ class Users:
 
     def delete_user(self, cursor):          # TODO set method deleting user
         pass
+
+
+# For testing only!
+from psycopg2 import connect
+
+test_case = Users('test_dummy', 'not a strong password')
+test_case_2 = Users('Dan', 'Some Random Passowrd')
+print(test_case_2.username)
+print(test_case_2.hashed_password)
+print(test_case_2._id)
+test_case.username = 'Marcin'
+test_case.hashed_password = 'my_test_password'
+print(test_case.hashed_password)
+print(test_case.username)
+print(test_case._id)
+
+try:
+    conn = connect(user="postgres", password="coderslab", host="localhost", dbname="message_db")
+    conn.autocommit = True
+    cursor = conn.cursor()
+    Users.save_to_db(test_case, cursor)
+    Users.save_to_db(test_case_2, cursor)
+except Exception as e:
+    print(e)
+
+print(test_case._id)
+print(test_case_2._id)
