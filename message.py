bb697868a39c0e7f28a83e24745b4fc0a4303b18
Imrie83 <czolgista83@gmail.com>
2021-11-05 10:29:04 +0100

    Created "send_message" function sending messages based on sender / receipent names

--- message.py
+++ message.py
@@ -9,7 +9,7 @@ HOST = 'localhost'
 DB = 'message_db'
 
 # TODO Add exception handling
-# TODO Add additionall functionality
+# TODO Add additional functionality
 # TODO create class to connecto to database!
 
 
@@ -25,26 +25,68 @@ def display_messages(username, password):
     user = Users.load_user_by_name(cursor, username)
 
     if not user:
+        conn.close()
         return 'User does not exist'
-
     elif not user.hashed_password == password:
+        conn.close()
         return 'Incorrect Password'
-
     else:
         messages = Messages.load_messages_by_sender_id(cursor, user.user_id)
-        if not messages:
-            return 'No messages from this user'
+        conn.close()
+    return messages
+
+
+def send_message(sender, password, receiver, message):
+
+    try:
+        conn = connect(user=USER, password=PASSWORD, host=HOST, dbname=DB)
+        conn.autocommit = True
+        cursor = conn.cursor()
+    except OperationalError as e:
+        return 'Connection error', e
+
+    sender = Users.load_user_by_name(cursor, sender)
+    receiver = Users.load_user_by_name(cursor, receiver)
+
+    if not sender:
+        conn.close()
+        return 'User does not exist'
+    elif not sender.hashed_password == password:
+        conn.close()
+        return 'Incorrect Password'
+    else:
+        if not receiver:
+            conn.close()
+            return 'Recipient does not exist'
+        elif len(message) > 255:
+            return "Message can't be longer than 255 characters"
         else:
-            for message in messages:
-                print(message)
+            to_send = Messages(sender.user_id, receiver.user_id)
+            to_send.text = message
+            to_send.save_to_db(cursor)
+            conn.close()
+            return 'Message sent successfully'
+
 
 
 # TODO: Set parser help on incorrect argument
 
 if __name__ == '__main__':
 
-    display_messages('test 5', 'asgdfs a3245')
-    print(display_messages('test', 'aewstfgasdf'))
+    m = (display_messages('test 5', 'asgdfs a3245'))
+    for i in m:
+        print(i)
+    n = display_messages('test', 'aewstfgasdf')
+    if not n:
+        print('No Messages')
+    else:
+        for i in n:
+            print(i)
+
+    print(send_message('test 5', 'asgdfs a3245', 'Marcin22',\
+                       'This is a random message to be sent lkasejf;l salkwasejro;awieujr sdaifo jawoiperu jsladk;fj \
+                       ao;weiu jraslkef ja;woij 3efoiwaejt flkjsdrhfg lkjaewh talw3ehf lksadrjawslkje htfgawoueeh \
+                       tiouwahefgkljsdarh glkjaershg askj hgfkljas hgsklaskej hfkljasdh s a sdaj'))
 
     # parser = argparse.ArgumentParser()
     # parser.add_argument('-u', '--username', help='username')
